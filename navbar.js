7772c233a145420659ac57d6d0c6d7cc94c3c8f7
MDZ2999 <mundoalexis1010@gmail.com>
2024-07-12 00:32:41 -0600

    arreglo recargar

--- navbar.js
+++ navbar.js
@@ -2,10 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // Obtener todos los elementos <a> dentro de <li> en el navbar
     const links = document.querySelectorAll('.nav-links li a');
 
-    // Establecer 'Inicio' como activo por defecto al cargar la página
-    document.getElementById('inicio-link').classList.add('active');
-
-    // Función para mostrar la sección correspondiente y ocultar las demás
+    // Función para mostrar la sección correspondiente y gestionar estado activo
     function showSection(targetId) {
         const targetSection = document.getElementById(targetId);
         // Mostrar solo la sección objetivo y ocultar las demás
@@ -13,10 +10,33 @@ document.addEventListener('DOMContentLoaded', function() {
             section.style.display = 'none';
         });
         targetSection.style.display = 'block';
+
+        // Guardar el ID de la sección activa en el almacenamiento local
+        localStorage.setItem('activeSectionId', targetId);
     }
 
-    // Mostrar la sección de inicio por defecto
-    showSection('inicio');
+    // Restaurar el estado activo al cargar la página
+    function restoreActiveSection() {
+        const activeSectionId = localStorage.getItem('activeSectionId');
+        if (activeSectionId) {
+            // Remover la clase 'active' de todos los elementos <li>
+            links.forEach(link => link.classList.remove('active'));
+
+            // Agregar la clase 'active' al elemento <li> correspondiente
+            const activeLink = document.querySelector(`.nav-links li a[data-section="${activeSectionId}"]`);
+            if (activeLink) {
+                activeLink.classList.add('active');
+                // Mostrar la sección correspondiente al ID guardado
+                showSection(activeSectionId);
+            }
+        } else {
+            // Mostrar la sección de inicio por defecto si no hay sección activa guardada
+            showSection('inicio');
+        }
+    }
+
+    // Mostrar la sección activa guardada al cargar la página
+    restoreActiveSection();
 
     // Iterar sobre cada elemento <a> para agregar evento de clic
     links.forEach(link => {
