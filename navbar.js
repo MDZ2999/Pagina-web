9fa2d4b46edc5cbfc018d9954947ffab16a68583
MDZ2999 <mundoalexis1010@gmail.com>
2024-07-13 19:48:48 -0600

    Recarga de pagina arreglada

--- navbar.js
+++ navbar.js
@@ -11,13 +11,23 @@ document.addEventListener('DOMContentLoaded', function() {
         });
         targetSection.style.display = 'block';
 
-        // Guardar el ID de la sección activa en el almacenamiento local
+        // Guardar el ID de la sección activa en el sessionStorage
+        sessionStorage.setItem('activeSectionId', targetId);
         localStorage.setItem('activeSectionId', targetId);
+
+        // Remover la clase 'active' de todos los elementos <li>
+        links.forEach(link => link.classList.remove('active'));
+
+        // Agregar la clase 'active' al elemento <li> correspondiente
+        const activeLink = document.querySelector(`.nav-links li a[data-section="${targetId}"]`);
+        if (activeLink) {
+            activeLink.classList.add('active');
+        }
     }
 
     // Restaurar el estado activo al cargar la página
     function restoreActiveSection() {
-        const activeSectionId = localStorage.getItem('activeSectionId');
+        const activeSectionId = sessionStorage.getItem('activeSectionId');
         if (activeSectionId) {
             // Remover la clase 'active' de todos los elementos <li>
             links.forEach(link => link.classList.remove('active'));
@@ -32,8 +42,18 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             // Mostrar la sección de inicio por defecto si no hay sección activa guardada
             showSection('inicio');
+            const inicioLink = document.querySelector(`.nav-links li a[data-section="inicio"]`);
+            if (inicioLink) {
+                inicioLink.classList.add('active');
+            }
         }
     }
+
+    // Ocultar todas las secciones al cargar la página
+    document.querySelectorAll('.section').forEach(section => {
+        section.style.display = 'none';
+        section.style.minHeight = '704px';  // Mantener la altura mínima durante la carga
+    });
 
     // Mostrar la sección activa guardada al cargar la página
     restoreActiveSection();
@@ -55,5 +75,10 @@ document.addEventListener('DOMContentLoaded', function() {
             // Mostrar la sección correspondiente
             showSection(targetId);
         });
+    });
+
+    // Verificar si la página fue cerrada
+    window.addEventListener('beforeunload', function() {
+        localStorage.removeItem('activeSectionId');
     });
 });
